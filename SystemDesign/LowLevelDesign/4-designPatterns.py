fe7e33b486b5c21e9e973d61d56f40f7a6970b9d
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-09-07 15:58:05 -0400

    Overview of Diesign Patterns

---
+++ SystemDesign/LowLevelDesign/4-designPatterns.py
@@ -0,0 +1,31 @@
+# Design Patterns are the best practices you didn't know you needed until you hit the same design poroblem for the third time.
+# They are reusable solutions to commonly occurring problems in software design. They provide a shared vocabulary, promote best practices, and help you build flexible, extensible, and maintainable systems.
+
+#============================================================================ Different Categrories of Design Patterns ===============================================================================#
+'''
+1. Creational Patterns (How Objects are Created)
+These patterns provide various object creation mechanisms, which increase flexibility and reuse of existing code.
+- Singleton: Ensure a class has only one instance.
+- Factory Method: Delegate object creation to subclasses.
+- Abstract Factory: Create families of related objects.
+- Builder: Construct complex objects step-by-step.
+- Prototype: Clone existing objects instead of creating new ones.
+
+
+2. Structural Patterns (How Objects are Composed)
+These patterns explain how to assemble objects and classes into larger structures while keeping these structures flexible and efficient.
+- Adapter: Makes one interface compatible with another.
+- Decorator: Adds new responsibilities to an object at runtime.
+- Facade: Provides a simplified interface to a complex system.
+- Composite: Treats individual and grouped objects uniformly.
+- Proxy: Acts as a placeholder or access control for another object.
+
+
+3. Behavioral Patterns (How Objects Interact)
+These patterns are concerned with algorithms and the assignment of responsibilities between objects.
+- Strategy: Enables selecting an algorithm at runtime.
+- Observer: Notifies dependent objects of state changes.
+- Command: Encapsulates a request as an object.
+- State: Allows an object to change behavior based on internal state.
+- Template Method: Defines the skeleton of an algorithm in a base class.
+'''
