580b9b51f1c2307a4d5ddd458a3adaf810f89d87
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-11-13 02:19:36 -0500

    Template for Answering Design Questions

---
+++ SystemDesign/LowLevelDesign/DesignA-Questions/SolutionTemplate.py
@@ -0,0 +1,53 @@
+#=========== Design a  ==============#
+
+'''
+# Define Requirements / Scope
+- 
+- 
+- 
+- 
+- 
+- 
+- 
+
+#==================== Create Classes and Define Methods ===================#
+class ()
+- 
+- 
+- 
+- 
+- 
+- 
+- 
+
+
+class ()
+- 
+- 
+- 
+- 
+
+
+class ()
+- 
+- 
+- 
+- 
+
+
+
+class ()
+-  
+-  
+-  
+
+
+
+
+class ()
+- 
+- 
+
+class ()
+- 
+'''
