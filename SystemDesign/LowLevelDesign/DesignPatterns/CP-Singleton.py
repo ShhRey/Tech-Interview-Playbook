5907e47685d4a92ac426653bfb2dc77adae950ae
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-12-30 03:58:32 -0500

    Fine-tuning Singleton Pattern

---
+++ SystemDesign/LowLevelDesign/DesignPatterns/CP-Singleton.py
@@ -0,0 +1,11 @@
+'''
+#=================================================== What is Singleton Pattern? =======================================================#
+Singleton Pattern is a creational design pattern that guarantees a class has only one instance and provides a global point of access to it.
+It involves only one class which is responsible for instantiating itself, making sure it creates not more than one instance.
+
+
+#=========================================== What Problems does Singleton Pattern Solve? ===============================================#
+The Singleton Pattern solves two problems at the same time, violating the Single Responsibility Principle:
+1. Ensure that every class has a Single Instance
+2. Provide Global Access to that Instance
+'''
