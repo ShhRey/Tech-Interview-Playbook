b23ecae6d154ef16e0f4f8eddc88be7873dc3cd5
Shrey Trivedi <shreytrivedi577@gmail.com>
2025-12-30 03:56:52 -0500

    Going deeper into Design Patterns

--- SystemDesign/LowLevelDesign/ImpConcepts/4-designPatterns.py
+++ SystemDesign/LowLevelDesign/ImpConcepts/4-designPatterns.py
@@ -1,8 +1,20 @@
 # Design Patterns are the best practices you didn't know you needed until you hit the same design poroblem for the third time.
-# They are reusable solutions to commonly occurring problems in software design. They provide a shared vocabulary, promote best practices, and help you build flexible, extensible, and maintainable systems.
+# They are reusable solutions to commonly occurring problems in software design. 
+# They provide a shared vocabulary, promote best practices, and help you build flexible, extensible, and maintainable systems.
+
+
+'''
+Why learn common Design Patterns?
+- Provide a Common Language: when talking to other developers, mentioning about these designs immediately communicates a shared understanding of the problem and its solution.
+- Prevent Reinventing the Wheel: You can leverage decades of experience from other developers instead of creating a less optimal solution from scratch.
+
+How to learn about Design Patterns?
+- Identify the problem: Understand the specific design problem the pattern is trying to solve.
+- Learn the solution: Study how the pattern solves that problem, including the classes, their relationships, and the purpose of each component.
+- Recognize examples: See the pattern in action in code you've already used, like a web framework or a library. This helps you understand its practical application.
+
 
 #============================================================================ Different Categrories of Design Patterns ===============================================================================#
-'''
 1. Creational Patterns (How Objects are Created)
 These patterns provide various object creation mechanisms, which increase flexibility and reuse of existing code.
 - Singleton: Ensure a class has only one instance.
