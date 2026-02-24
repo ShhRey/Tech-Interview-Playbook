0c8e936c3be3c3b30c406a7511d1623a47241b53
Shrey Trivedi <shreytrivedi577@gmail.com>
2026-02-24 13:14:13 -0500

    Modifying Details for Graphs

--- DSA/DataStructures/graphs.py
+++ DSA/DataStructures/graphs.py
@@ -23,6 +23,7 @@ Different Types of Graphs:
 - Unweighted Graphs: All edges are treated equally.
 - Connected Graphs: Graphs allow traversal from any node to any other. 
 - Disconnected Graphs: Graphs have isolated components.
+- Vertex Labelled Graphs: Each node is associated with a label or identifier in addition to its data. Example: transportation network (city, coordinates)
 - Cyclic/Acyclic: Cyclic graphs contain cycles; acyclic graphs do not. 
 => Directed acyclic graphs (DAGs) are significant for tasks like topological sorting.
 
@@ -32,7 +33,10 @@ Different Ways to Represent Graphs:
 
 #================================================================== Graph Representation in Programming? ==================================================================#
 1. Adjacency List (Dynamic-sized Array / LinkedList)
-It represents a graph as an array of linked lists, where each list contains the vertices adjacent to a given vertex.
+An AL is an array or dictionary that stores all the neighbors of each node.
+There are two ways to implement adjacency lists:
+- As an array, where each index represents a node and the list stored at that index contains its neighbors.
+- As a dictionary, where each key represents a node and the value associated to that key (a list) contains its neighbors.
 
 What are the Problems?
 - To check for an edge from Vertex (U) to Vertex (V), one has to traverse all neighbors. TC: O(V)
@@ -43,7 +47,7 @@ What are the Benefits?
 
 
 2. Adjacency Matrix
-It is a way of representing a graph as a matrix of boolean 0's and 1's. 
+It is a way of representing a graph as a matrix of boolean 0's and 1's. The values in the matrix represent the edges or connections between the nodes.
 - The size of the matrix will be: V x V (v is the number of vertices).
 - The value for mat[i][j] = 1, mat[j][i] = 1 only if there is an edge going from vertex i to vertex j or visa-versa.
 - For an Undirected Graph: Matrix will always be Symmetrical. It may or may not be symmetric for directed graphs.
@@ -72,6 +76,13 @@ Rules:
 - When you are selecting a vertex for exploration, you must visit all its adjacent vertices, before proceeding to next vertex for exploration!
 - You should select the next vertex for exploration from the queue only!
 
+Algorithm works like this:
+- You start at a specific node.
+- That node is marked as visited and added to the queue.
+- While the queue is not empty, the current node is removed from the queue (dequeued). 
+- Then, for each one of its neighbors, if the neighbor has not been visited, it is marked as visited and added to the queue.
+=> When the queue is empty, the traversal is complete.
+
 BFS is used for:
 - Finding shortest paths in unweighted graphs (number of edges)
 - Level-wise computations in trees/graphs
@@ -84,7 +95,14 @@ BFS is used for:
 '''
 #=============================================================== What is a DFS? (pre-order) =================================================================#
 DFS is the “diving deep” approach to graph traversal. It explores each branch as far as possible before backtracking.
-It uses stack data structure for traversal. We can select any vertex as the starting point, 
+It uses stack data structure for traversal. We can select any vertex as the starting point.
+
+The algorithm works like this:
+- Start at a specific node.
+- That node is marked as visited and added to the stack.
+- While the stack is not empty, the current node is popped (removed). This is when we "visit" or process it (for example, by printing its value). 
+- Then, all of its unvisited neighbors are marked as visited and added to the stack.
+=> When the stack is empty, the traversal is completed and all nodes have been visited.
 
 Rules:
 - When you visit a vertex, forget about other not visited vertices, start exploring visited vertex!
