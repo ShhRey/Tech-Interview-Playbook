4d2f10092c7868c00191ec739b8b0b6cbdcafbfd
Shrey Trivedi <shreytrivedi577@gmail.com>
2026-05-07 18:57:17 -0400

    Diff ways of solving ques 144

---
+++ CodingPlatforms/LeetCode/Trees/144-PreorderTraversalBT.py
@@ -0,0 +1,68 @@
+# Question: Given the root of a binary tree, return the preorder traversal of its nodes' values.
+# PreOrder Traversal: node -> left -> right
+
+
+from typing import List, Optional
+
+# Defining a Tree Node
+class TreeNode:
+    # Providing default values for left, right, nodeVal
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    
+class Solution:
+    # Brute Force based Approach
+    def preorderTraversalBF(self, root: Optional[TreeNode]) -> List[int]:
+        # Store node values 
+        res = []
+        # Helper Func
+        def dfs(node):
+            # Check if node present
+            if not node:
+                return
+            # Store node val into res
+            res.append(node.val)
+            # recursively follow preOrder
+            dfs(node.left)
+            dfs(node.right)
+        # Call Helper wrt Root
+        dfs(root)
+        # Provide node vals
+        return res
+    
+
+
+    # Stack based Approach
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # Store node values 
+        res, stack = [], []
+        # Initialize Root
+        curr = root
+        # Check until curr/stack not empty
+        while curr or stack:
+            # Keep Going Left until Node
+            while curr:
+                # Append nodeVal to res
+                res.append(curr.val)
+                # Add node to stack
+                stack.append(curr)
+                # Move curr to left
+                curr = curr.left
+            # Store stack top to curr
+            curr = stack.pop()
+            # Check for nodes on right
+            curr = curr.right
+        # Provide node vals
+        return res
+
+
+
+
+
+# Custome Test Cases
+q = Solution()
+q1 = q.preorderTraversalBF([1,'null',2,3])
+print(q1)
